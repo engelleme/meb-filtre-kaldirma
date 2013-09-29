30e8b06658df8e2c5d85ea533e4b67bbcdd5bed6
engelleme <wrolaipo@uroid.com>
2013-09-29 19:58:27 +0300

    Create header.php
    MEB Engeli Kaldır, MEB Filtresi Kır.

---
+++ header.php
@@ -0,0 +1,20 @@
+	<tr>
+		<td width="728" colspan="2" height="90">
+			<?php include( "ads_728x90.php" ); ?>
+		
+		</td>
+	</tr>
+	
+	<tr>
+		<td width="728" colspan="2" height="29" align="center" bgcolor="#FFFF00">
+			<b>
+			<font size="2" face="Arial Black">
+			<a href="<?php echo $site_url; ?>">anasayfa</a> | 
+			<a href="hakkinda.php">hakkında</a> | 
+			<a href="kullanim_kosullari.php">sözleşme</a> | 
+			<a href="indir.php">indir</a> | 
+			<a href="iletisim.php">iletişim</a>
+			</font>
+			</b>
+		</td>
+	</tr>
